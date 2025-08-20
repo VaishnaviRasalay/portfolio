4d7a2bbbf6837611968cea8797b0855f30221247
Vaishnavi <rasalayvaishnavi@gmail.com>
2025-08-20 10:43:15 +0530

    Updated Home component and resume PDF to open in new tab

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,25 +3,28 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   // Function to open resume in a new tab
-const openResume = () => {
-  window.open(process.env.PUBLIC_URL + "/assets/VaishnaviResume.pdf", "_blank", "noopener,noreferrer");
-}
+
 
 
   return (
     <div className='home-container'>
-      <img 
+      <img className='image'
         src={`${process.env.PUBLIC_URL}/assets/female.jpg`} 
         alt="Vaishnavi Rasalay" 
       />
 
       <h2><i>Vaishnavi Rasalay</i></h2>
       <p className='designation'>- Frontend Developer</p>
+<a
+  href={`${process.env.PUBLIC_URL}/assets/vaishnaviresume.pdf`}
+  target="_blank"
+  rel="noopener noreferrer"
+  className="btn"
+>
+  Open Resume
+</a>
 
-      {/* Button to open resume */}
-      <button onClick={openResume} className="btn">
-        Download Resume
-      </button>
+
 
       <p className='description'>
         I create beautiful, responsive websites using React, JavaScript, HTML, and CSS.
@@ -30,6 +33,7 @@ const openResume = () => {
       <div>
         <Link to="/Projects" className="btn">View Projects</Link>
         <Link to="/Contact" className="btn">Contact Me</Link>
+
       </div>
     </div>
   );
